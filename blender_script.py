f618cb6154518682f0179703d26ab5d74e589e19
Ihor <avoqun@gmail.com>
2026-05-29 10:41:25 +0300

    add camera parameters that affect fov and aspect ratio

--- blender_script.py
+++ blender_script.py
@@ -3,8 +3,14 @@ import bpy
 # use cam
 scene = bpy.context.scene
 cam = bpy.data.objects.get("DroneCamera")
-scene.camera = cam
 
+# camera settings
+cam.data.lens = 6.9 # mm 
+cam.data.sensor_fit = 'VERTICAL' 
+cam.data.sensor_height = 60 # mm 
+scene.render.resolution_x = 1200
+scene.render.resolution_y = 1000
+scene.camera = cam
 
 # switch to rendered 
 area = [a for a in bpy.context.screen.areas if a.type == 'VIEW_3D'][0]
@@ -12,6 +18,10 @@ space = area.spaces.active
 space.shading.type = 'RENDERED'
 bpy.context.scene.render.engine = 'BLENDER_EEVEE'
 space.region_3d.view_perspective = 'CAMERA'
+
+# renderer settings
+scene.render.use_motion_blur = True
+scene.render.motion_blur_shutter = 5.0   # length of blur; bigger = more
 
 # camera zoom 
 cam.data.show_passepartout = True
@@ -31,15 +41,23 @@ import time
 import math 
 import threading 
 
+# def save_drone_render_on_update(drone):
+#     f = scene.frame_current + 1
+#     drone.keyframe_insert("location", frame=f)     # prev key at f-1 still exists
+#     scene.frame_set(f)
+#     scene.render.filepath = f"/tmp/frame_{f:05d}.png"
+#     bpy.ops.render.render(write_still=True)
+
 def mainloop():
-  # example moving & rotating object 
   i = 0 
   while True:
     i += 1
     time.sleep(1/60)
     drone = bpy.data.objects["Drone"]
     drone.location = (math.sin(i/10)*5, math.cos(i/10)*5, 5)
-    drone.rotation_euler = (math.sin(i/10), 0.0, 0.0)   # x, y, z; degrees→radians
+    drone.rotation_euler = (math.sin(i/10)/2, 0.0, 0.0)   # x, y, z; degrees→radians
+    # save_drone_render_on_update(drone)
+
 
 t = threading.Thread(target=mainloop)
 t.start()
