a56d58b59b5793de3c62fc6cb4cb68cd4787e213
yash4enk0 <oleksandra.yash4enk0@gmail.com>
2026-06-02 18:23:15 +0300

    fixes

--- examples/tx12_read_as_hid_joystic_example.py
+++ examples/tx12_read_as_hid_joystic_example.py
@@ -1,26 +1,28 @@
-import hid # pip isntall hid 
-import struct 
+import struct
 
-found = None
+import hid
+
+found = False
+device = None
 for device in hid.enumerate():
-  if "TX12" in device['product_string']:
-    found = device['vendor_id'], device['product_id']
-    break
+    print(device)
+    if "TX12" in device["product_string"]:
+        found = True
+        break
 
 if not found:
-  print("tx12 not found")
-  exit()
+    print("tx12 not found")
+    exit()
 
-tx12 = hid.Device(found[0], found[1])
-tx12.nonblocking = False
+tx12 = hid.Device(device["vendor_id"], device["product_id"])
 
 while True:
-  data = tx12.read(64)
+    data = tx12.read(64)
+    btn1, btn2, btn3, x, y, z, rx, ry, rz, s1, s2 = struct.unpack("<BBBHHHHHHHH", data)
+    axes = [x, y, z, rx, ry, rz, s1, s2]
+    axes_norm = [(val - 1024) / 1024.0 for val in axes]  # -1.0 to +1.0
 
-  btn1, btn2, btn3, x, y, z, rx, ry, rz, s1, s2 = struct.unpack('<BBBHHHHHHHH', data)
-  axes = [x, y, z, rx, ry, rz, s1, s2]
-  axes_norm = [(val - 1024) / 1024.0 for val in axes]  # -1.0 to +1.0
-
-  print(f"Axes norm    : {[f'{a:.3f}' for a in axes_norm]}")
-  print(f"Buttons high : {bin(btn1)[2:].zfill(8)} {bin(btn2)[2:].zfill(8)} {bin(btn3)[2:].zfill(8)}")
-
+    print(f"Axes norm    : {[f'{a:.3f}' for a in axes_norm]}")
+    print(
+        f"Buttons high : {bin(btn1)[2:].zfill(8)} {bin(btn2)[2:].zfill(8)} {bin(btn3)[2:].zfill(8)}"
+    )
--- mav2blender_socket_bridge.py
+++ mav2blender_socket_bridge.py
@@ -1,23 +1,20 @@
 import math
+import socket
+import struct
 import time
 
 from pymavlink import mavutil
 
-conn_str = "127.0.0.1:14560"
-master = mavutil.mavlink_connection(conn_str)
-master.wait_heartbeat()
-print("Heartbeat done")
 
-
-def set_interval(message_id, rate):
-    frequency = int(1e6 / rate)
+def set_interval(message_id, frequency_hz):
+    rate_ms = int(1e6 / frequency_hz)
     master.mav.command_long_send(
         master.target_system,
         master.target_component,
         mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
         0,
         message_id,
-        frequency,
+        rate_ms,
         0,
         0,
         0,
@@ -26,27 +23,27 @@ def set_interval(message_id, rate):
     )
 
 
-set_interval(mavutil.mavlink.MAVLINK_MSG_ID_LOCAL_POSITION_NED, 20)
-set_interval(mavutil.mavlink.MAVLINK_MSG_ID_AHRS2, 20)
-print("Requested the message succesfully")
-
-import socket
-import struct
+conn_str = "127.0.0.1:14560"
+master = mavutil.mavlink_connection(conn_str)
+master.wait_heartbeat()
+print("Heartbeat done")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ("0.0.0.0", 6781)
 
+set_interval(mavutil.mavlink.MAVLINK_MSG_ID_LOCAL_POSITION_NED, 20)
+set_interval(mavutil.mavlink.MAVLINK_MSG_ID_AHRS2, 20)
+print("Requested first msg successfully")
+
 tstart = time.time()
+
 
 try:
     x, y, z, roll_rad, pitch_rad, yaw_rad = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
     while True:
-        msg = master.recv_match(
-            type=["LOCAL_POSITION_NED", "AHRS2"], blocking=True
-        )  # .to_dict()
+        msg = master.recv_match(type=["LOCAL_POSITION_NED", "AHRS2"], blocking=True)
         if msg._type == "LOCAL_POSITION_NED":
             x, y, z = msg.x, msg.y, msg.z
-            print("xyz", msg)
             sock.sendto(
                 struct.pack(
                     "!" + "d" * 6,
@@ -59,17 +56,18 @@ try:
                 ),
                 addr,
             )
+            print("NED\n", msg)
 
         if msg._type == "AHRS2":
             roll_rad, pitch_rad, yaw_rad = msg.roll, msg.pitch, msg.yaw
-            print("ahrs", msg)
+            print("AHRS\n", msg)
 
         if time.time() - tstart > 0.1:
             set_interval(mavutil.mavlink.MAVLINK_MSG_ID_LOCAL_POSITION_NED, 30)
             set_interval(mavutil.mavlink.MAVLINK_MSG_ID_AHRS2, 30)
             tstart = time.time()
+
 except KeyboardInterrupt:
     if "master" in locals():
         master.close()
         print("MAVLink connection closed.")
-    pass
