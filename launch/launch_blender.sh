bbce4ed2a4faaae7cfa7fdcfb16a4b92870d5016
Ihor <avoqun@gmail.com>
2026-06-17 23:12:51 +0300

    latest

---
+++ launch/launch_blender.sh
@@ -0,0 +1,1 @@
+blender ../src/blender_scenes/scene-simple.blend --python ../src/blender_scripts/move_camera_position_from_socket.py -w -p 0 0 1280 720
--- launch/launch_sitl.sh
+++ launch/launch_sitl.sh
@@ -1,4 +1,3 @@
 source ../enviroments/current.sh
 cd ../submodules/ardupilot/
-python3 Tools/autotest/sim_vehicle.py --console -v ArduCopter --out=udp:127.0.0.1:14560 --out=udp:127.0.0.1:14561 --out=udp:127.0.0.1:14562
-
+python3 Tools/autotest/sim_vehicle.py --console -v ArduCopter --out=udp:127.0.0.1:14560 --out=udp:127.0.0.1:14561 --out=udp:127.0.0.1:14562 --mavproxy-args="--streamrate=100"
